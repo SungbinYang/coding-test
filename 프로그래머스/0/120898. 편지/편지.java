132b163b2f4f845301e90463081c80388b2aeda4
SungBin Yang (Robert) <ysb05222@naver.com>
2025-07-07 17:38:00 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,12 +1,5 @@
 class Solution {
     public int solution(String message) {
-        char[] chars = message.toCharArray();
-        int length = 0;
-        
-        for (char ch : chars) {
-            length++;
-        }
-        
-        return length * 2;
+        return message.length() * 2;
     }
 }
