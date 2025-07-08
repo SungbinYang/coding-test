9a295aeba1c6f5e67d42ab40ad1bf8ad75c29d4a
SungBin Yang (Robert) <ysb05222@naver.com>
2025-07-08 14:26:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.46 ms, Memory: 86.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (String str : rsp.split("")) {
+            if (str.equals("2")) {
+                sb.append("0");
+            } else if (str.equals("0")) {
+                sb.append("5");
+            } else if (str.equals("5")) {
+                sb.append("2");
+            }
+        }
+        
+        return sb.toString();
+    }
+}
