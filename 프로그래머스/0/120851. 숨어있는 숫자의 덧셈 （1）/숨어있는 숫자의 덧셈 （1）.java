cba7b7d88d9a797211c54bf5b486cc6c6f3cd5bd
SungBin Yang (Robert) <ysb05222@naver.com>
2025-07-08 14:56:40 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.27 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string) {
+        char[] chars = my_string.toCharArray();
+        List<Integer> numbers = new ArrayList<>();
+        
+        for (char ch : chars) {
+            String str = String.valueOf(ch);
+            if (str.matches("[0-9]+")) {
+                numbers.add(Integer.valueOf(str));
+            }
+        }
+        
+        return numbers.stream()
+            .mapToInt(i -> i)
+            .sum();
+            
+    }
+}
